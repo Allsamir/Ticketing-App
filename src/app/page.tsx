ff473c00b82f33383c03f928f6e4288589ecbd20
Al Samir <shahinur1591@gmail.com>
2024-06-20 18:02:00 +0600

    trying to build learn how to build and then try to deploy this repo to vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,9 @@ import Ticket from "@/interface/TicketInterface";
 
 const getAllTheTickets = async () => {
   try {
-    const res = await fetch(
-      `https://ticketing-app-beta-beryl.vercel.app/api/tickets`,
-      {
-        cache: "no-store",
-      },
-    );
+    const res = await fetch(`http://localhost:3000/api/tickets`, {
+      next: { revalidate: 2 },
+    });
     return res.json();
   } catch (error) {
     console.log(error);
@@ -27,18 +24,19 @@ const groupTicketByCategory = (tickets: Ticket[]) => {
 };
 
 export default async function Home() {
-  const { tickets } = await getAllTheTickets();
+  const { tickets = [] } = await getAllTheTickets();
   const groupedTickets = groupTicketByCategory(tickets);
+  console.log(groupedTickets);
   return (
     <main>
       {Object.entries(groupedTickets).map(
         ([category, tickets], index: number) => (
           <>
-            <h2 className="m-2 capitalize" key={index}>
+            <h2 className="m-2 capitalize" key={tickets[0]._id}>
               {category}
             </h2>
             <div
-              key={index}
+              key={category}
               className="gap-6 my-4 grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2"
             >
               {tickets.map((ticket: Ticket, index: number) => (
